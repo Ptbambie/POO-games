435a05e42325042b7caa69e3e93fa70b9652fa21
Ptbambie <audreyalaime3@gmail.com>
2023-08-20 16:06:01 +0200

    set up projectiles

--- index.js
+++ index.js
@@ -8,39 +8,95 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-// Dessine un cercle dans le canvas
-ctx.beginPath();
-ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
-// Les paramètres de la fonction arc sont :
-// - x et y : les coordonnées du centre du cercle
-// - 30 : le rayon du cercle
-// - 0 et Math.PI * 2 : les angles de début et de fin du cercle (dessine un cercle complet)
-// - false : indique de dessiner dans le sens des aiguilles d'une montre
-ctx.fillStyle = "red"; // Définit la couleur de remplissage du cercle
-ctx.fill(); // Remplit le cercle avec la couleur définie
-
-// Dessine un rectangle dans le canvas
-class Entity{
-    constructor(x, y , radius){
+// Classe de base pour les entités dessinables
+class Entity {
+    constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.color = "red";
-    }
-
-    draw(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        ctx.fillStyle = this.color; // Définit la couleur de remplissage du cercle
-        ctx.fill(); // Remplit le cercle avec la couleur définie
-}
-}
-class Player extends Entity{
-    constructor(x, y , radius){
-        super(x, y , radius);
         this.color = color;
     }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color; // Définit la couleur de remplissage de l'entité
+        ctx.fill(); // Remplit l'entité avec la couleur définie
+    }
 }
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
-Player.draw();
+// Classe pour le joueur
+class Player extends Entity {
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color);
+    }
+}
+
+// Classe pour les projectiles avec la propriété velocity (vitesse)
+class Projectile extends Entity {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color);
+        this.velocity = velocity;
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+// Classe pour les ennemis
+class Enemy extends Projectile {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color, velocity);
+    }
+}
+
+// Liste des projectiles
+const projectiles = [];
+
+// Liste des ennemis
+const enemies = [];
+
+// Créer le joueur
+const player = new Player(canvas.width / 2, canvas.height / 2, 10, "blue");
+
+// Animation du projectile lors du clic
+window.addEventListener("click", (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    );
+
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5,
+    };
+
+    const projectile = new Projectile(
+        player.x,
+        player.y,
+        5,
+        "white",
+        velocity
+    );
+    projectiles.push(projectile); // Ajoute le projectile à la liste des projectiles
+});
+
+// Animation du jeu
+function animate() {
+    requestAnimationFrame(animate);
+
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.draw();
+
+    projectiles.forEach((projectile) => projectile.update());
+
+    enemies.forEach((enemy) => enemy.update());
+}
+
+// Démarrer l'animation
+animate();
